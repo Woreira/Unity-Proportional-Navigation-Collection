c7fb3e6821db6f2433cda6dedb4733be2c488475
Woreira <moreira_fernandes@outlook.com>
2021-11-08 03:04:35 -0300

    Update Missile.cs
    cleanup

--- ProportionalNavDemo/Assets/Scripts/Missile.cs
+++ ProportionalNavDemo/Assets/Scripts/Missile.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//implement acceleration
 public class Missile : MonoBehaviour{
 
     public GameObject explosionPrefab;
